83a2c017fae4d63ef04fb259986b5f52854d572f
Noam Manos <nmanos@redhat.com>
2021-01-24 19:05:42 +0200

    create locals

--- osp-scripts/main.tf
+++ osp-scripts/main.tf
@@ -1,21 +1,29 @@
 locals {
-  ipsec_natt_port = 4501
-  ipsec_ike_port = 501
+  cluster_id           = "cluster_id"
+  ipsec_natt_port      = 4501
+  ipsec_ike_port       = 501
+  os_auth_url          = "https://openstack-auth-url:13000/v3"
+  os_username          = "openstack-user"
+  os_password          = "openstack-pass"
+  os_user_domain_name  = "openstack.domain"
+  os_tenant_name       = "openstack-project"
+  os_project_domain_id = "openstack-project-id"
+  os_region            = "openstack-region"
 }
 
 provider "openstack" {
-  auth_url         = var.OS_AUTH_URL
-  user_name        = var.OS_USERNAME
-  password         = var.OS_PASSWORD
-  user_domain_name = var.OS_USER_DOMAIN_NAME
-  tenant_name      = var.OS_PROJECT_NAME
-  project_domain_id = var.OS_PROJECT_DOMAIN_ID
-  region           = var.OS_REGION_NAME
+  auth_url          = local.os_auth_url
+  user_name         = local.os_username
+  password          = local.os_password
+  user_domain_name  = local.os_user_domain_name
+  tenant_name       = local.os_tenant_name
+  project_domain_id = local.os_project_domain_id
+  region            = local.os_region
 }
 
 module "ocp-osp-prep" {
-  source     = "./tf-scripts"
-  cluster_id = "cluster_id"
+  source          = "./tf-scripts"
+  cluster_id      = local.cluster_id
   ipsec_natt_port = local.ipsec_natt_port
-  ipsec_ike_port = local.ipsec_ike_port
+  ipsec_ike_port  = local.ipsec_ike_port
 }
