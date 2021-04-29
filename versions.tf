ea8887736e9c70877eac768c2e2e60b12bfbc6a4
Noam Manos <nmanos@redhat.com>
2021-04-29 16:18:31 +0300

    Add required_providers for openstack

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
     required_version = ">= 0.12, <= 0.12.12"
+
+    required_providers {
+        openstack = {
+        source  = "terraform-provider-openstack/openstack"
+        version = "~> 1.35.0"
+        }
+    }
+    
 }
